8ee3014cf876ce4f0010ab661bc1819191bdd2c5
Arkagit <66682580+Arkagit@users.noreply.github.com>
2025-06-08 09:40:57 +0530

    Update
    Update

--- Sensor localization/Plots.R
+++ Sensor localization/Plots.R
@@ -10,16 +10,49 @@ add_legend <- function(...) {
   legend(...)
 }
 
-RAM = matrix(0, nrow = nsim, ncol = 1)
-Metro = matrix(0, nrow = nsim, ncol = 1)
+RAM_d = list(matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet),
+			matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet))
+RAM_test = list(matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet),
+			matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet))
+Metro_d = list(matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet),
+			matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet))
+RAM_J_Barker_d = list(matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet),
+			matrix(0, nrow = nsim, ncol = repet),matrix(0, nrow = nsim, ncol = repet))
 
-for(i in 1:nsim){
-  RAM[i, ] = prod(1 - colMeans(alpha_list[[i]][[1]]))
-  Metro[i, ] = prod(1 - colMeans(alpha_list[[i]][[2]]))
+
+for(k in 1:4){
+	for(i in 1:nsim){
+		for(j in 1:repet){
+			RAM_d[[k]][i, j] = alpha_list[[i]][[2]][[j]][,k]
+            Metro_d[[k]][i, j] = alpha_list[[i]][[4]][[j]][,k]
+            RAM_J_Barker_d[[k]][i, j] = alpha_list[[i]][[1]][[j]][,k]
+            RAM_test[[k]][i, j] = alpha_list[[i]][[3]][[j]][,k]
+	    }
+	}
 }
 
-pdf(paste("lb_hist.pdf"), height = 6, width = 16)
-par(mfrow = c(1, 2))
-hist(RAM, xlab = paste("Lower Bound"), main  = "RAM", breaks = 20, ylim = c(0, 20))
-hist(Metro, xlab = paste("Lower Bound"), main  = "MH", breaks = 20, ylim = c(0, 20))
+
+RAM = (1 - rowMeans(RAM_d[[1]]))*(1 - rowMeans(RAM_d[[2]]))*
+				(1 - rowMeans(RAM_d[[3]]))*(1 - rowMeans(RAM_d[[4]]))
+
+RAM_test = (1 - rowMeans(RAM_test[[1]]))*(1 - rowMeans(RAM_test[[2]]))*
+				(1 - rowMeans(RAM_test[[3]]))*(1 - rowMeans(RAM_test[[4]]))
+	
+Metro = (1 - rowMeans(Metro_d[[1]]))*(1 - rowMeans(Metro_d[[2]]))*
+				(1 - rowMeans(Metro_d[[3]]))*(1 - rowMeans(Metro_d[[4]]))
+
+RAM_J_Barker = (1 - rowMeans(RAM_J_Barker_d[[1]]))*(1 - rowMeans(RAM_J_Barker_d[[2]]))*
+				(1 - rowMeans(RAM_J_Barker_d[[3]]))*(1 - rowMeans(RAM_J_Barker_d[[4]]))
+
+
+pdf(paste("lb_RAM_Metro.pdf"), height = 8, width = 8)
+#par(mfrow = c(1, 2))
+plot(rowMeans(jumping.scale), RAM, ylab = "Lower Bounds", xlab = "Jump Scale", type = "l", col = "blue", lwd = "3")
+lines(rowMeans(jumping.scale), Metro)
+lines(rowMeans(jumping.scale), RAM_J_Barker, col = "green")
+lines(rowMeans(jumping.scale), RAM_test, col = "brown")
+legend("bottomright", legend = c("RAM", "RAM (Barker with same chain)", "MH", "RAM (Joint Barker rate)"),
+ col = c("blue", "brown", "black", "green"), lty = 1, cex = 1.5)
 dev.off()
+
+
--- Sensor localization/RAM.R
+++ Sensor localization/RAM.R
@@ -103,6 +103,7 @@ ram.kernel <- function(current.location, current.aux, loc.number, scale) {
   log.z.c.den <- l.target(z.c, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
   z.c.den <- exp(log.z.c.den)
 
+
   # downhill
   x.p1 <- x.c
   x.p1[(2 * loc.number - 1) : (2 * loc.number)] <- x.p1[(2 * loc.number - 1) : (2 * loc.number)] + 
@@ -125,7 +126,7 @@ ram.kernel <- function(current.location, current.aux, loc.number, scale) {
                                                    rnorm(2, 0, scale)
   log.x.p2.den <- l.target(x.p2, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
   x.p2.den <- exp(log.x.p2.den)
-  print(log.x.p2.den)
+  
   N.u <- 1
   while (-rexp(1) > log(x.p2.den + eps) - log(x.p1.den + eps)) {
     x.p2 <- x.p1
@@ -156,11 +157,14 @@ ram.kernel <- function(current.location, current.aux, loc.number, scale) {
   min.nu <- min(1, (x.c.den + eps) / (z.c.den + eps))
   min.de <- min(1, (x.p2.den + eps) / (z.den + eps))
 
-  if (is.infinite(log.x.p2.den) && is.infinite(log.x.c.den) && log.x.p2.den == log.x.c.den) {
-    l.mh <- log(min.nu) - log(min.de)  # or 0, depending on your context
-    } else {
-    l.mh <- log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)
-  }
+  l.mh <- log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)
+
+  #if (is.infinite(log.x.p2.den) && is.infinite(log.x.c.den) && log.x.p2.den == log.x.c.den) {
+   # l.mh <- log(min.nu) - log(min.de)  # or 0, depending on your context
+   # print(1)
+   # } else {
+   # l.mh <- log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)
+  #}
 
   if (l.mh > - rexp(1)) {
     x.c <- x.p2
@@ -169,14 +173,144 @@ ram.kernel <- function(current.location, current.aux, loc.number, scale) {
   }
 
   acceptance_rate = min(1, exp(l.mh))
+  accp_barker = exp(l.mh)/(1 + exp(l.mh))
 
-  c(x.c, z.c, N.d, N.u, N.dz, accept, acceptance_rate)
+  c(x.c, z.c, N.d, N.u, N.dz, accept, acceptance_rate, accp_barker)
 }
 
 MHwG.RAM <- function(initial.loc, initial.aux, jump.scale, 
              Ob, Os, Xb, Xs, Yb, Ys, n.sample = 10, n.burn = 10) {
 
-  print(Sys.time())
+  
+  n.total <- n.sample + n.burn
+  accept <- matrix(0, nrow = n.total, ncol = 4)
+  alpha = matrix(0, nrow = n.total, ncol = 4)
+  alpha_bark = matrix(0, nrow = n.total, ncol = 4)
+  out <- matrix(NA, nrow = n.total, ncol = 8)
+  loc.t <- initial.loc
+  aux.t <- initial.aux
+  Nd <- matrix(NA, nrow = n.total, ncol = 4)
+  Nu <- matrix(NA, nrow = n.total, ncol = 4)
+  Nz <- matrix(NA, nrow = n.total, ncol = 4)
+  
+  for (i in 1 : n.total) {
+    for (j in 1 : 4) {
+      TEMP <- ram.kernel(loc.t, aux.t, j, jump.scale[j])
+      loc.t <- TEMP[1 : 8]
+      aux.t <- TEMP[9 : 16]
+      Nd[i, j] <- TEMP[17]
+      Nu[i, j] <- TEMP[18]
+      Nz[i, j] <- TEMP[19]
+      accept[i, j] <- TEMP[20]
+      alpha[i, j] = TEMP[21]
+      alpha_bark[i, j] = TEMP[22]
+    }
+    out[i, ] <- loc.t
+  }
+  
+  list(x = out[-c(1 : n.burn), ], 
+       accept = accept[-c(1 : n.burn), ],
+       N.d = Nd[-c(1 : n.burn), ],
+       N.u = Nu[-c(1 : n.burn), ],
+       N.z = Nz[-c(1 : n.burn), ],
+       alpha_RAM = alpha,
+       alpha_bark = alpha_bark
+       )
+
+}
+
+######## RAM Joint Barker
+
+ram.j.barker.kernel <- function(current.location, current.aux, loc.number, scale) {
+
+  eps <- 10^(-100)
+  accept <- 0
+  x.c <- current.location 
+  log.x.c.den <- l.target(x.c, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  x.c.den <- exp(log.x.c.den)
+  z.c <- current.aux
+  log.z.c.den <- l.target(z.c, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  z.c.den <- exp(log.z.c.den)
+
+
+  # downhill
+  x.p1 <- x.c
+  x.p1[(2 * loc.number - 1) : (2 * loc.number)] <- x.p1[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                   rnorm(2, 0, scale)
+  log.x.p1.den <- l.target(x.p1, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  x.p1.den <- exp(log.x.p1.den)
+  N.d <- 1
+  while (-rexp(1) > log(x.c.den + eps) - log(x.p1.den + eps)) {
+    x.p1 <- x.c
+    x.p1[(2 * loc.number - 1) : (2 * loc.number)] <- x.p1[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                     rnorm(2, 0, scale)
+    log.x.p1.den <- l.target(x.p1, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+    x.p1.den <- exp(log.x.p1.den)
+    N.d <- N.d + 1
+  }
+
+  # uphill
+  x.p2 <- x.p1
+  x.p2[(2 * loc.number - 1) : (2 * loc.number)] <- x.p2[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                   rnorm(2, 0, scale)
+  log.x.p2.den <- l.target(x.p2, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  x.p2.den <- exp(log.x.p2.den)
+  
+  N.u <- 1
+  while (-rexp(1) > log(x.p2.den + eps) - log(x.p1.den + eps)) {
+    x.p2 <- x.p1
+    x.p2[(2 * loc.number - 1) : (2 * loc.number)] <- x.p2[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                     rnorm(2, 0, scale)
+    log.x.p2.den <- l.target(x.p2, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+    x.p2.den <- exp(log.x.p2.den)
+    N.u <- N.u + 1
+  }
+  
+  # downhill for N.d
+  N.dz <- 1     # number of total downhill trials for estimate
+  z <- x.p2
+  z[(2 * loc.number - 1) : (2 * loc.number)] <- z[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                rnorm(2, 0, scale)
+  log.z.den <- l.target(z, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  z.den <- exp(log.z.den)
+  while (-rexp(1) > log(x.p2.den + eps) - log(z.den + eps)) {
+    z <- x.p2
+    z[(2 * loc.number - 1) : (2 * loc.number)] <- z[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                  rnorm(2, 0, scale)
+    log.z.den <- l.target(z, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+    z.den <- exp(log.z.den)
+    N.dz <- N.dz + 1
+  }
+
+  # accept or reject the proposal
+  min.nu <- min(1, (x.c.den + eps) / (z.c.den + eps))
+  min.de <- min(1, (x.p2.den + eps) / (z.den + eps))
+
+  l.mh <- (log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)) -
+          log(1 + exp(log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)))
+
+  #if (is.infinite(log.x.p2.den) && is.infinite(log.x.c.den) && log.x.p2.den == log.x.c.den) {
+   # l.mh <- log(min.nu) - log(min.de)  # or 0, depending on your context
+   # print(1)
+   # } else {
+   # l.mh <- log.x.p2.den + log(min.nu) - log.x.c.den - log(min.de)
+  #}
+
+  if (l.mh > - rexp(1)) {
+    x.c <- x.p2
+    z.c <- z
+    accept <- 1
+  }
+
+  acceptance_rate = exp(l.mh)
+
+  c(x.c, z.c, N.d, N.u, N.dz, accept, acceptance_rate)
+}
+
+MHwG.RAM.J.Barker <- function(initial.loc, initial.aux, jump.scale, 
+             Ob, Os, Xb, Xs, Yb, Ys, n.sample = 10, n.burn = 10) {
+
+  
   n.total <- n.sample + n.burn
   accept <- matrix(0, nrow = n.total, ncol = 4)
   alpha = matrix(0, nrow = n.total, ncol = 4)
@@ -200,17 +334,18 @@ MHwG.RAM <- function(initial.loc, initial.aux, jump.scale,
     }
     out[i, ] <- loc.t
   }
-  print(Sys.time())
+  
   list(x = out[-c(1 : n.burn), ], 
        accept = accept[-c(1 : n.burn), ],
        N.d = Nd[-c(1 : n.burn), ],
        N.u = Nu[-c(1 : n.burn), ],
        N.z = Nz[-c(1 : n.burn), ],
-       alpha_RAM = alpha)
+       alpha_RAM_J_Barker = alpha
+       )
 
 }
 
-j.scale <- rep(1.08, 4)
+#j.scale <- rep(1.08, 4)
 #system.time(res.ram <- MHwG.RAM(runif(8), runif(8), jump.scale = j.scale, 
 #                                Ob, Os, Xb, Xs, Yb, Ys, 
 #                                n.sample = 100, n.burn = 100))
@@ -240,11 +375,13 @@ Metro.kernel <- function(current.location, loc.number, jump.scale) {
   l.new = l.target(x.p, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
   l.current = l.target(current.location, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
 
-  if (is.infinite(l.new) && is.infinite(l.current) && l.new == l.current) {
-    l.metro <- 0  # or 0, depending on your context
-    } else {
-    l.metro <- l.new - l.current
-  }
+  #if (is.infinite(l.new) && is.infinite(l.current) && l.new == l.current) {
+  #  l.metro <- 0  # or 0, depending on your context
+  #  } else {
+  #  l.metro <- l.new - l.current
+  #}
+
+  l.metro <- l.new - l.current
 
   if (l.metro >  -rexp(1)) {
     current.location <- x.p
@@ -258,7 +395,6 @@ Metro.kernel <- function(current.location, loc.number, jump.scale) {
 
 MHwG.Metro <- function(initial.loc, jump.scale, Ob, Os, Xb, Xs, Yb, Ys, n.sample = 10, n.burn = 10) {
 
-  print(Sys.time())
   n.total <- n.sample + n.burn
   accept <- matrix(0, nrow = n.total, ncol = 4)
   out <- matrix(NA, nrow = n.total, ncol = 8)
@@ -276,12 +412,102 @@ MHwG.Metro <- function(initial.loc, jump.scale, Ob, Os, Xb, Xs, Yb, Ys, n.sample
 
   }
 
-  print(Sys.time())
   list(x = out[-c(1 : n.burn), ], accept = accept[-c(1 : n.burn), ], alpha_Metro = accept)
 
 }
 
-j.scale <- rep(1.08, 4)
+#j.scale <- rep(1.08, 4)
+
+############################# RAM BARKER
+two_coin_algorithm <- function(c.xy, mu, mu.p, loc.number, scale) {
+  repeat {
+    # Step 1: Draw C1 ~ Bern(cy / (cx + cy))
+    C1 <- rbinom(1, 1, 1/(1 + c.xy))
+    
+    if (C1 == 1) {
+      # Step 3: Draw C2 ~ Bern(py)
+      M <- mu
+      M[(2 * loc.number - 1) : (2 * loc.number)] <- M[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                   rnorm(2, 0, scale)
+      py = min(1, (exp(l.target(mu, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)) + eps) /
+             (exp(l.target(M, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)) + eps))
+
+      C2 <- rbinom(1, 1, py)
+      if (C2 == 1) {
+        return(1)  # Output 1
+      } 
+      # else repeat from beginning
+    } else {
+      M <- mu.p
+      M[(2 * loc.number - 1) : (2 * loc.number)] <- M[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                   rnorm(2, 0, scale)
+      px = min(1, (exp(l.target(mu.p, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)) + eps) /
+             (exp(l.target(M, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)) + eps))
+      # Step 9: Draw C2 ~ Bern(px)
+      C2 <- rbinom(1, 1, px)
+      if (C2 == 1) {
+        return(0)  # Output 0
+      }
+      # else repeat from beginning
+    }
+  }
+}
+
+
+ram.barker.kernel = function(current.location, loc.number, scale, simnum){
+  eps <- 10^(-100)
+  check <- rep(0, simnum)
+  x.c <- current.location 
+  log.x.c.den <- l.target(x.c, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  x.c.den <- exp(log.x.c.den)
+  
+
+
+  # downhill
+  x.p <- x.c
+  x.p[(2 * loc.number - 1) : (2 * loc.number)] <- x.p[(2 * loc.number - 1) : (2 * loc.number)] + 
+                                                   rnorm(2, 0, scale)
+  log.x.p.den <- l.target(x.p, 0.3, 0.02, Ob, Os, Xb, Xs, Yb, Ys)
+  x.p.den <- exp(log.x.p.den)
+
+  c.xy <- exp(log.x.c.den - log.x.p.den)
+  
+  for(s in 1:simnum){
+    check[s] = two_coin_algorithm(c.xy, x.c, x.p, loc.number, scale)
+  }
+  
+  check_prob = mean(check); check_prob
+
+  if(rbinom(1, 1, check_prob) == 1){
+    x.c = x.p
+  }
+
+  return(c(x.c, check_prob))
+}
+
+
+
+MHwG.RAM.Barker <- function(initial.loc, jump.scale, 
+             Ob, Os, Xb, Xs, Yb, Ys, n.sample = 10, n.burn = 10) {
+
+  n.total <- n.sample + n.burn
+  alpha = matrix(0, nrow = n.total, ncol = 4)
+  out <- matrix(NA, nrow = n.total, ncol = 8)
+  loc.t <- initial.loc
+  
+  for (i in 1 : n.total) {
+    for (j in 1 : 4) {
+      TEMP <- ram.barker.kernel(loc.t, j, jump.scale[j], simnum)
+      loc.t <- TEMP[1 : 8]
+      alpha[i, j] = TEMP[9]
+    }
+    out[i, ] <- loc.t
+  }
+  
+  list(x = out[-c(1 : n.burn), ], 
+       alpha_RAM_Barker = alpha
+       )
+}
 # The sample size used in the article is 
 # "sample.size = 1967150" and "burn.size = 20000".
 
@@ -293,38 +519,63 @@ j.scale <- rep(1.08, 4)
 
 ###################### Experiment with MH
 
-repet = 1e2
-nsim = 1e2
-j.scale <- rep(1.08, 4)
-
+repet = 1e4
+nsim = 5e1
+simnum = 1e2
+#j.scale <- rep(1.08, 4)
+jumping.scale = matrix(c(seq(0.01, 4, length.out = nsim), seq(0.01, 4, length.out = nsim),
+                  seq(0.01, 4, length.out = nsim), seq(0.01, 4, length.out = nsim)), 
+                    nrow = nsim, byrow = FALSE)
 
 parallel::detectCores()
-n.cores <- 10
+n.cores <- 4
 doParallel::registerDoParallel(cores = n.cores)
 
 #alpha_list = foreach(k = 1:nsim)%dopar%{
-
+initial.loc = c(0.5748, 0.0991, 0.2578, 0.8546, 
+               0.9069, 0.3651, 0.1350, 0.0392)
 alpha_list = list(0)
 
-for(k in 1:nsim){
-  initial.loc = runif(8)
+alpha_list = foreach(k = 1:nsim)%dopar%{
+  print(k)
+  j.scale = jumping.scale[k,]
 
-  alpha_mat_MH = matrix(0, nrow = repet, ncol = 4)
-  alpha_mat_RAM = matrix(0, nrow = repet, ncol = 4)
+  mat_RAM_J_Barker = list()
+  mat_MH = list()
+  mat_RAM = list()
+  mat_RAM_bark = list()
+  #mat_RAM_Barker = list()
 
   for(i in 1:repet){
-    alpha_mat_RAM[i, ] = MHwG.RAM(initial.loc, runif(8), jump.scale = j.scale, 
+    mat_RAM_J_Barker[[i]] = MHwG.RAM.J.Barker(initial.loc, runif(8), jump.scale = j.scale, 
                                   Ob, Os, Xb, Xs, Yb, Ys, 
-                                  n.sample = 1, n.burn = 0)$alpha_RAM
-    alpha_mat_MH[i, ] = MHwG.Metro(initial.loc, jump.scale = j.scale, 
+                                  n.sample = 1, n.burn = 0)$alpha_RAM_J_Barker
+
+    test = MHwG.RAM(initial.loc, runif(8), jump.scale = j.scale, 
+                                  Ob, Os, Xb, Xs, Yb, Ys, 
+                                  n.sample = 1, n.burn = 0)
+    mat_RAM[[i]] = test$alpha_RAM
+
+    mat_RAM_bark[[i]] = test$alpha_bark
+
+    mat_MH[[i]] = MHwG.Metro(initial.loc, jump.scale = j.scale, 
                                    Ob, Os, Xb, Xs, Yb, Ys, n.sample = 1, n.burn = 0)$alpha_Metro
+    
+    #mat_RAM_Barker[[i]] = MHwG.RAM.Barker(initial.loc, jump.scale = j.scale, 
+    #                              Ob, Os, Xb, Xs, Yb, Ys, 
+    #                              n.sample = 1, n.burn = 0)$alpha_RAM_Barker
+
+    print(i)
   }
 
-  out = list(alpha_mat_RAM, alpha_mat_MH)
-  alpha_list[[k]] = out
+  out = list(mat_RAM_J_Barker, mat_RAM, mat_RAM_bark, mat_MH) #, mat_RAM_Barker)
+  out
 }
 
 
-save(repet, nsim, alpha_list, file = "RAM_data.Rdata")
+
+
+
+save(repet, nsim, simnum, jumping.scale, alpha_list, file = "RAM_data.Rdata")
 
 
