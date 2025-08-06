4389fc6aaf634cf3a534e7f6bdf38eefc5de4dd2
Arkagit <66682580+Arkagit@users.noreply.github.com>
2025-08-06 11:34:16 +0530

    Update
    Update

--- Sensor localization/ESS_ESJD/dummy.R
+++ Sensor localization/ESS_ESJD/dummy.R
@@ -1,4 +1,4 @@
-set.seed(1234)  
+set.seed(11)  
 
 library(foreach)
 library(doParallel)
@@ -37,7 +37,7 @@ Ys <- matrix(c(0, 0, 0, 0.9266,
                0, 0.2970, 0, 0,
                0.9266, 0.8524, 0, 0), ncol = 4)
 
-eps = 10^(-100)
+eps = 10^(-308)
 
 
 samp_size = 1e6
@@ -89,19 +89,19 @@ par(mfrow = c(2,2), mar = c(6, 8, 6, 4))
 
 plot(CRAM_data[,1], CRAM_data[,2], type = "p", xlab = "x11", ylab = "x12"
   , main = "Location of x1", xlim = range(CRAM_data[,1]), ylim = range(CRAM_data[,2]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAM_data[,3], CRAM_data[,4], type = "p", xlab = "x21", ylab = "x22"
   , main = "Location of x2", xlim = range(CRAM_data[,3]), ylim = range(CRAM_data[,4]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAM_data[,5], CRAM_data[,6], type = "p", xlab = "x31", ylab = "x32"
   , main = "Location of x3", xlim = range(CRAM_data[,5]), ylim = range(CRAM_data[,6]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAM_data[,7], CRAM_data[,8], type = "p", xlab = "x41", ylab = "x42"
   , main = "Location of x4", xlim = range(CRAM_data[,7]), ylim = range(CRAM_data[,8]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 dev.off()
 
@@ -111,19 +111,19 @@ par(mfrow = c(2,2), mar = c(6, 8, 6, 4))
 
 plot(CMH_data[,1], CMH_data[,2], type = "p", xlab = "x11", ylab = "x12"
   , main = "Location of x1", xlim = range(CMH_data[,1]), ylim = range(CMH_data[,2]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CMH_data[,3], CMH_data[,4], type = "p", xlab = "x21", ylab = "x22"
   , main = "Location of x2", xlim = range(CMH_data[,3]), ylim = range(CMH_data[,4]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CMH_data[,5], CMH_data[,6], type = "p", xlab = "x31", ylab = "x32"
   , main = "Location of x3", xlim = range(CMH_data[,5]), ylim = range(CMH_data[,6]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CMH_data[,7], CMH_data[,8], type = "p", xlab = "x41", ylab = "x42"
   , main = "Location of x4", xlim = range(CMH_data[,7]), ylim = range(CMH_data[,8]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 dev.off()
 
@@ -133,19 +133,19 @@ par(mfrow = c(2,2), mar = c(6, 8, 6, 4))
 
 plot(CRAB_data[,1], CRAB_data[,2], type = "p", xlab = "x11", ylab = "x12"
   , main = "Location of x1", xlim = range(CRAB_data[,1]), ylim = range(CRAB_data[,2]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAB_data[,3], CRAB_data[,4], type = "p", xlab = "x21", ylab = "x22"
   , main = "Location of x2", xlim = range(CRAB_data[,3]), ylim = range(CRAB_data[,4]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAB_data[,5], CRAB_data[,6], type = "p", xlab = "x31", ylab = "x32"
   , main = "Location of x3", xlim = range(CRAB_data[,5]), ylim = range(CRAB_data[,6]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 plot(CRAB_data[,7], CRAB_data[,8], type = "p", xlab = "x41", ylab = "x42"
   , main = "Location of x4", xlim = range(CRAB_data[,7]), ylim = range(CRAB_data[,8]),
-  cex.lab = 3,cex.main = 3,cex.axis = 2)
+  cex.lab = 3,cex.main = 3,cex.axis = 2, cex = 2)
 
 dev.off()
 
