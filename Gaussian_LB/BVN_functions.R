8d14ec2eed54644df26002fd297dd4a2b27a2ef9
Arkagit <66682580+Arkagit@users.noreply.github.com>
2025-10-21 14:46:52 +0530

    Update
    Update

--- Gaussian_LB/BVN_functions.R
+++ Gaussian_LB/BVN_functions.R
@@ -1,21 +1,22 @@
-# proposal pdf for block MH
-q = function(y1, x1, prop_cov){
-	out1 = exp(-log(2* pi) - log(det(prop_cov))/2 - t(y1 - x1)%*%solve(prop_cov)%*%(y1 - x1)/2)
+# log proposal pdf for block MH
+log.q = function(y1, x1, prop_cov){
+	out1 = - t(y1 - x1)%*%solve(prop_cov)%*%(y1 - x1)/2
 	return(out1)
 }
 
 # target pdf for block MH
-f = function(x2, tar_mean, tar_cov){
-	out2 = exp(-log(2* pi) - log(det(tar_cov))/2 - t(x2 - tar_mean)%*%solve(tar_cov)%*%(x2 - tar_mean)/2)
+log.f = function(x2, tar_mean, tar_cov){
+	out2 = - t(x2 - tar_mean)%*%solve(tar_cov)%*%(x2 - tar_mean)/2
 	return(out2)
 }
 
 # Block \alpha_B
 alpha_B = function(x_init, tar_mean, tar_cov, prop_cov){
 		
-	y = c(rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
+	#y = rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
+	y = rnorm(2, 0, sqrt(2))
 
-	r = exp(log(f(y, tar_mean, tar_cov)) + log(q(x_init, x_init, prop_cov)) - log(f(x_init, tar_mean, tar_cov)) - log(q(y, x_init, prop_cov)))
+	r = exp(log.f(y, tar_mean, tar_cov) + log.q(x_init, x_init, prop_cov) - log.f(x_init, tar_mean, tar_cov) - log.q(y, x_init, prop_cov))
 
 	if(r < 1){
 		alpha = r
@@ -23,19 +24,19 @@ alpha_B = function(x_init, tar_mean, tar_cov, prop_cov){
 		alpha = 1
 	}
 
-	return(alpha)
+	return(r)
 }
 
 
 # CMH1 \alpha_mh
-alpha_mh = function(x_init, tar_mean, tar_cov, prop_cov){
+alpha_mh = function(x_init, x1, tar_mean, tar_cov, prop_cov){
 		
 	y = rnorm(1, x_init[2], sqrt(prop_cov[2,2]))
 
-	r = exp(log(dnorm(y, x_init[2] + tar_cov[1,2]*x_init[1]/tar_cov[1, 1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]))) +
-	    log(dnorm(x_init[2], y, sqrt(prop_cov[2,2]))) -
-		 log(dnorm(x_init[2], x_init[2] + tar_cov[1, 2]*x_init[1]/tar_cov[1,1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]))) - 
-		 log(dnorm(y, x_init[2], sqrt(prop_cov[2,2]))))
+	r = exp(dnorm(y, x_init[2] + tar_cov[1,2]*x_init[1]/tar_cov[1, 1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]), log = TRUE) +
+		     dnorm(x_init[2], y, sqrt(prop_cov[2,2]), log = TRUE) -
+			 dnorm(x_init[2], x_init[2] + tar_cov[1, 2]*x_init[1]/tar_cov[1,1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]), log = TRUE) - 
+			 dnorm(y, x_init[2], sqrt(prop_cov[2,2]), log = TRUE))
 
 	if(r < 1){
 		alpha = r
@@ -43,26 +44,31 @@ alpha_mh = function(x_init, tar_mean, tar_cov, prop_cov){
 		alpha = 1
 	}
 
-	return(alpha)
+	return(r)
 }
 
 
 # Proximal Augmented \alpha_p
 
-alpha_p = function(x_init, tar_mean, tar_cov, prop_cov, lambda){
+log.f2 = function(x_init, z, inv_tar_cov, prop_cov, lambda){
+	out2 = - t(x_init)%*%(inv_tar_cov + diag(c(1, 1))/ lambda)%*%x_init/2 + t(z)%*%x_init/lambda
+	return(out2)
+}
 
-	Omega = solve(solve(tar_cov) + diag(c(1, 1))/lambda)
+alpha_p = function(x_init, z, tar_cov, prop_cov, lambda){
 
-	z = c(rnorm(1, x_init[1], sqrt(lambda)), rnorm(1, x_init[2], sqrt(lambda)))
+	inv_tar_cov = solve(tar_cov)
 
-	pop_mean = Omega%*%z/lambda
+	#dummy = c(rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
+	y = rnorm(2, 0, sqrt(2))
 
-	dummy = c(rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
+	r = exp(log.f2(y, z, inv_tar_cov, prop_cov, lambda) + log.q(x_init, x_init, prop_cov) - 
+		log.f2(x_init, z, inv_tar_cov, prop_cov, lambda) - log.q(y, x_init, prop_cov))
 
-	r = exp(- 0.5*t(dummy - pop_mean)%*%solve(Omega)%*%(dummy - pop_mean) +
-				0.5*t(x_init - x_init)%*%solve(Omega)%*%(x_init - x_init) - 
-				0.5*t(x_init - pop_mean)%*%solve(prop_cov)%*%(x_init - pop_mean) +
-				0.5*t(dummy - x_init)%*%solve(prop_cov)%*%(dummy - x_init))
+	# r = exp(- 0.5*t(y - pop_mean)%*%Omega_inv%*%(y - pop_mean) +
+	# 			0.5*t(x_init - x_init)%*%solve(prop_cov)%*%(x_init - x_init) - 
+	# 			0.5*t(x_init - pop_mean)%*%solve(prop_cov)%*%(x_init - pop_mean) +
+	# 			0.5*t(y - x_init)%*%Omega_inv%*%(y - x_init))
 
 	if(r < 1){
 		alpha = r
@@ -70,6 +76,11 @@ alpha_p = function(x_init, tar_mean, tar_cov, prop_cov, lambda){
 		alpha = 1
 	}
 
-	return(alpha)
+	return(r)
 }
+
+
+
+
+
 
