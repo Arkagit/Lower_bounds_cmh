c78a2ca90280296e2645f7ad06e02dba4165968e
Arkagit <66682580+Arkagit@users.noreply.github.com>
2025-10-22 19:30:42 +0530

    Update
    Update

--- Gaussian_LB/BVN_functions.R
+++ Gaussian_LB/BVN_functions.R
@@ -10,73 +10,64 @@ log.f = function(x2, tar_mean, tar_cov){
 	return(out2)
 }
 
-# Block \alpha_B
-alpha_B = function(x_init, tar_mean, tar_cov, prop_cov){
+# Block \alpha_block
+alpha_block = function(x_init, tar_mean, tar_cov, prop_cov){
 		
-	#y = rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
-	y = rnorm(2, 0, sqrt(2))
+	y = rnorm(2, x_init, sqrt(diag(prop_cov)))
 
-	r = exp(log.f(y, tar_mean, tar_cov) + log.q(x_init, x_init, prop_cov) - log.f(x_init, tar_mean, tar_cov) - log.q(y, x_init, prop_cov))
+	ratio = exp(log.f(y, tar_mean, tar_cov) - log.f(x_init, tar_mean, tar_cov))
 
-	if(r < 1){
-		alpha = r
+	if(ratio < 1){
+		alpha = ratio
 	}else{  
 		alpha = 1
 	}
 
-	return(r)
+	return(alpha)
 }
 
 
-# CMH1 \alpha_mh
-alpha_mh = function(x_init, x1, tar_mean, tar_cov, prop_cov){
+# CMH1 \alpha_cmh
+alpha_cmh = function(x_init, x1, tar_mean, tar_cov){
 		
-	y = rnorm(1, x_init[2], sqrt(prop_cov[2,2]))
+	curr = c(x1, x_init[2])
 
-	r = exp(dnorm(y, x_init[2] + tar_cov[1,2]*x_init[1]/tar_cov[1, 1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]), log = TRUE) +
-		     dnorm(x_init[2], y, sqrt(prop_cov[2,2]), log = TRUE) -
-			 dnorm(x_init[2], x_init[2] + tar_cov[1, 2]*x_init[1]/tar_cov[1,1], sqrt(tar_cov[2,2] - (tar_cov[1, 2]^(2))/tar_cov[1,1]), log = TRUE) - 
-			 dnorm(y, x_init[2], sqrt(prop_cov[2,2]), log = TRUE))
+	prop = c(x1, rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
 
-	if(r < 1){
-		alpha = r
+	ratio = exp(log.f(prop, tar_mean, tar_cov) - log.f(curr, tar_mean, tar_cov))
+
+	if(ratio < 1){
+		alpha = ratio
 	}else{  
 		alpha = 1
 	}
 
-	return(r)
+	return(alpha)
 }
 
 
 # Proximal Augmented \alpha_p
 
-log.f2 = function(x_init, z, inv_tar_cov, prop_cov, lambda){
-	out2 = - t(x_init)%*%(inv_tar_cov + diag(c(1, 1))/ lambda)%*%x_init/2 + t(z)%*%x_init/lambda
+log.f2 = function(x_init, z, tar_mean, tar_cov, lambda){
+
+	out2 = log.f(x_init, tar_mean, tar_cov) - (1/(2 * lambda))*(sum((z - x_init)^2))
+	
 	return(out2)
 }
 
-alpha_p = function(x_init, z, tar_cov, prop_cov, lambda){
+alpha_proximal = function(x_init, z, tar_mean, tar_cov, prop_cov, lambda){
 
-	inv_tar_cov = solve(tar_cov)
+	y = rnorm(2, x_init, sqrt(diag(prop_cov)))
 
-	#dummy = c(rnorm(1, x_init[1], sqrt(prop_cov[1,1])), rnorm(1, x_init[2], sqrt(prop_cov[2,2])))
-	y = rnorm(2, 0, sqrt(2))
+	ratio = exp(log.f2(y, z, tar_mean, tar_cov, lambda) - log.f2(x_init, z, tar_mean, tar_cov, lambda))
 
-	r = exp(log.f2(y, z, inv_tar_cov, prop_cov, lambda) + log.q(x_init, x_init, prop_cov) - 
-		log.f2(x_init, z, inv_tar_cov, prop_cov, lambda) - log.q(y, x_init, prop_cov))
-
-	# r = exp(- 0.5*t(y - pop_mean)%*%Omega_inv%*%(y - pop_mean) +
-	# 			0.5*t(x_init - x_init)%*%solve(prop_cov)%*%(x_init - x_init) - 
-	# 			0.5*t(x_init - pop_mean)%*%solve(prop_cov)%*%(x_init - pop_mean) +
-	# 			0.5*t(y - x_init)%*%Omega_inv%*%(y - x_init))
-
-	if(r < 1){
-		alpha = r
+	if(ratio < 1){
+		alpha = ratio
 	}else{  
 		alpha = 1
 	}
 
-	return(r)
+	return(alpha)
 }
 
 
--- Gaussian_LB/BV_normal.R
+++ Gaussian_LB/BV_normal.R
@@ -4,25 +4,18 @@ source("BVN_functions.R")
 
 library(MASS)
 library(latex2exp)
-library(foreach)
-library(doParallel)
-library(matrixcalc)
+# library(foreach)
+# library(doParallel)
+# library(matrixcalc)
 
 # rho values
 rho = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.999)
 
 ## Number of iteration of A's
-n = 1e3
-
-## Number of iteration of \alpha's
-m = 1e2
-
-## Initialization
-A_mh = rep(0, m)
-A_p = rep(0, m)
+n = 1e4
 
 ## Auxiliary variances
-lambda = c(1, 10)
+lambda = c(0.1, 1, 10)
 
 ## Target mean of X
 tar_mean = c(0, 0)
@@ -32,9 +25,10 @@ x_init = c(0, 0)
 z_init = c(0, 0)
 
 ## Initializations for acceptance probs and avg acceptance probs
-A_B = matrix(0, nrow = n, ncol = length(rho))
-A_s_mh = matrix(0, nrow = n, ncol = length(rho))
-A_s_p = list(matrix(0, nrow = n, ncol = length(rho)), matrix(0, nrow = n, ncol = length(rho)))
+A_block = matrix(0, nrow = n, ncol = length(rho))
+A_star_cmh = matrix(0, nrow = n, ncol = length(rho))
+A_star_proximal = list(matrix(0, nrow = n, ncol = length(rho)), matrix(0, nrow = n, ncol = length(rho)),
+	matrix(0, nrow = n, ncol = length(rho)))
 
 ## Proposal covariance matrix
 prop_cov = matrix(c(2, 0, 0, 2), nrow = 2)
@@ -48,40 +42,46 @@ for(i in 1:length(rho)){
 
 	for(j in 1:n){
 
-		A_B[j, i] = alpha_B(x_init, tar_mean, tar_cov, prop_cov)
+		A_block[j, i] = alpha_block(x_init, tar_mean, tar_cov, prop_cov)
 
 		#A_mh[j, i] = alpha_mh(x_init, tar_mean, tar_cov, prop_cov)
 
-		for(k in 1:m){
-			x1 = rnorm(1, x_init[1] + tar_cov[2,1]*x_init[2]/tar_cov[2, 2], sqrt(tar_cov[1,1] - (tar_cov[2, 1]^(2))/tar_cov[2,2]))
-			A_mh[k] = alpha_mh(x_init, x1, tar_mean, tar_cov, prop_cov)
-		}
+		# for(k in 1:m){
+		# 	x1 = rnorm(1, x_init[1] + tar_cov[2,1]*x_init[2]/tar_cov[2, 2], sqrt(tar_cov[1,1] - (tar_cov[2, 1]^(2))/tar_cov[2,2]))
+		# 	A_star_cmh[k] = alpha_cmh(x_init, x1, tar_mean, tar_cov, prop_cov)
+		# }
+		x1 = rnorm(1, x_init[1], prop_cov[1, 1])
 
-		A_s_mh[j, i] = mean(A_mh)
+		A_star_cmh[j, i] = alpha_cmh(x_init, x1, tar_mean, tar_cov) #mean(A_mh)
 
 		for(k in 1:length(lambda)){
 
-			for(l in 1:m){
-				z = rnorm(2, x_init, sqrt(lambda[k]))
-				A_p[k] = alpha_p(x_init, z, tar_cov, prop_cov, lambda[k])
-			}
+			z = rnorm(2, x_init, sqrt(lambda[k]))
 
-			A_s_p[[k]][j, i] = mean(A_p)
+			A_star_proximal[[k]][j, i] = alpha_proximal(x_init, z, tar_mean, tar_cov, prop_cov, lambda[k])
+
+			# for(l in 1:m){
+			# 	z = rnorm(2, x_init, sqrt(lambda[k]))
+			# 	A_p[k] = alpha_proximal(x_init, z, tar_cov, prop_cov, lambda[k])
+			# }
+
+			# A_s_p[[k]][j, i] = mean(A_p)
 		}
 		print(i)
 		print(j)
 	}
-	list(A_B, A_s_mh, A_s_p)
+	list(A_block, A_star_cmh, A_star_proximal)
 }
 
 
 pdf("Gaussian_Lower_bound.pdf", height = 6, width = 8)
 par(mar = c(5.1, 4.8, 4.1, 2.1))
-plot(rho, 1 - colMeans(A_B), type = 'b', col = "black", ylim = c(0,1), ylab = "Estimated Lower Bounds", xlab = expression(rho))
-lines(rho, 1 - colMeans(A_s_p[[1]]), type = 'b', col = "blue")
-lines(rho, 1 - colMeans(A_s_p[[2]]), type = 'b', col = "brown")
-lines(rho, 1 - colMeans(A_s_mh), type = 'b', col = "red")
-legend("bottomright", bty = "n", legend = c("Complete Block MH", "CMH1", expression("Augmented CMH ("* lambda* "= 1)"), expression("Augmented CMH ("* lambda* "= 10)")), 
-	col = c("black", "red", "blue", "brown"), lty = 1, cex=0.65)
+plot(rho, 1 - colMeans(A_block), type = 'b', col = "black", ylim = c(0,1), ylab = "Estimated Lower Bounds", xlab = expression(rho))
+lines(rho, 1 - colMeans(A_star_proximal[[1]]), type = 'b', col = "blue")
+lines(rho, 1 - colMeans(A_star_proximal[[2]]), type = 'b', col = "brown")
+lines(rho, 1 - colMeans(A_star_proximal[[3]]), type = 'b', col = "purple")
+lines(rho, 1 - colMeans(A_star_cmh), type = 'b', col = "red")
+legend("bottomright", bty = "n", legend = c("Complete Block MH", "CMH1", expression("Augmented CMH ("* lambda* "= 0.1)"), expression("Augmented CMH ("* lambda* "= 1)"),
+	expression("Augmented CMH ("* lambda* "= 10)")), col = c("black", "red", "blue", "brown", "purple"), lty = 1, cex=0.65)
 dev.off()
 
