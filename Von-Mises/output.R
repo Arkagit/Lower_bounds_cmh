100d54f5557c4a9799dfae35a30e39ef74789e16
Arkagit <66682580+Arkagit@users.noreply.github.com>
2026-08-17 16:46:31 +0530

    update
    update

--- Von-Mises/output.R
+++ Von-Mises/output.R
@@ -3,37 +3,89 @@ set.seed(1234)
 source("functions.R")
 
 c0 = 0.68
-a = 1
-b = 10
+a = 0.1
+b = 1
+h_choices = 4
 
-h = c(0.3, 0.5, 1, 5, 10, 20, 50, 100)
+dd_psi(100)
+
 n = c(5e2, 7e2, 1e3, 3e3, 5e3, 1e4, 5e4, 1e5)
+h = matrix(0, nrow = h_choices, ncol = length(n))
+lb_out = matrix(0, nrow = h_choices, ncol = length(n))
 
-pairs_labels = paste("(", h, ",\n", 
-                     ifelse(n >= 1000, paste(format(n/1000, digits=1), "k", sep=""), n), 
-                     ")", sep = "")
+h[1, ] = 5
+h[2, ] = 5/2
+h[3, ] = 5/log(n)
+h[4, ] = 5/n
 
-lb_out = vm_lb(n, h, a, b)
-x_pos = 1:length(pairs_labels)
+for(i in 1:4){
+    lb_out[i,] = vm_lb(n, h[i,], a, b)
+}
 
-pdf("VM_lower_bound.pdf", height = 7, width = 9)
+lb_min = min(lb_out)
+lb_max = max(lb_out)
 
-par(mar = c(5.1, 4.1, 4.1, 2.1))
+pdf("VM_lower_bound.pdf", height = 8, width = 9)
 
-plot(x_pos, lb_out,
+# Increase bottom margin for x-axis labels + legend
+par(mar = c(8, 6, 5, 4))
+
+x <- seq_along(n)
+
+plot(x, lb_out[1, ],
      type = "b",
      xaxt = "n",
      pch = 16,
      col = "steelblue",
-     lwd = 2,
-     xlab = "(h, n) pairs",
+     lwd = 2.5,
+     cex = 1.1,
+     ylim = c(lb_min, lb_max),
+     xlab = "Data Size",
      ylab = "Lower Bounds",
-     cex.lab = 1.2,
-     cex.main = 1.3)
+     cex.lab = 1.5,
+     cex.axis = 1.3)
 
-axis(1, at = x_pos, labels = pairs_labels, las = 1, cex.axis = 1, 
-     tck = -0.02, padj = 0.5, mgp = c(3, 0.5, 0))
+axis(1,
+     at = x,
+     labels = c("500", "700", "1K", "3K",
+                "5K", "10K", "50K", "100K"),
+     cex.axis = 1.3)
 
-grid(lty = 2, col = "gray")
+lines(x, lb_out[2, ],
+      type = "b",
+      pch = 16,
+      col = "darkolivegreen",
+      lwd = 2.5,
+      cex = 1.1)
+
+lines(x, lb_out[3, ],
+      type = "b",
+      pch = 16,
+      col = "purple",
+      lwd = 2.5,
+      cex = 1.1)
+
+lines(x, lb_out[4, ],
+      type = "b",
+      pch = 16,
+      col = "brown",
+      lwd = 2.5,
+      cex = 1.1)
+
+#grid(lty = 2, col = "gray")
+
+legend("right",
+       legend = c(
+         expression(h == 5),
+         expression(h == 5/2),
+         expression(h == 5/log(n)),
+         expression(h == 5/n)
+       ),
+       col = c("steelblue", "darkolivegreen", "purple", "brown"),
+       pch = 16,
+       lty = 1,
+       lwd = 2.5,
+       bty = "n",
+       cex = 1.1)
 
 dev.off()
